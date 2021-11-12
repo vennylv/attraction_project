208af67ed2fcb0cf787ee15774701b122fbe6eef
vennylv <vennylv@gmail.com>
2021-11-12 17:40:32 +0900

    first commit

---
+++ attraction_project/attraction/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'attraction/index.html')
+
+
+def attraction_page(request):
+    return render(request, 'attraction/attraction.html')
